a1b9e4d3163fb3667a9e6c7c71e4d21c03ef0594
Henrik Kiepe <hkiepe@inkontor.com>
2020-03-05 20:37:55 +0100

    Login Form

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,16 +1,18 @@
-import React, { Component } from "react";
-import withStyles from "@material-ui/core/styles/withStyles";
-import Link from "react-router-dom/Link";
+import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 // MUI Suff
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 
 const styles = {
   card: {
-    display: "flex",
+    display: 'flex',
     marginBottom: 20
   },
   image: {
@@ -24,6 +26,7 @@ const styles = {
 
 class Scream extends Component {
   render() {
+      dayjs.extend(relativeTime)
     const {
       classes,
       scream: {
@@ -54,7 +57,7 @@ class Scream extends Component {
               {userHandle}
             </Typography>
             <Typography variant="body2" color="textSecondary">
-              {createdAt}
+              {dayjs(createdAt).fromNow()}
             </Typography>
             <Typography variant="body1">{body}</Typography>
           </CardContent>
