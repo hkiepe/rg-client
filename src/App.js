18b53ab26b553c0227e50718a67acf8cfd26ac52
Henrik Kiepe <hkiepe@inkontor.com>
2020-03-05 14:07:12 +0100

    Scream Card Details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 // Components
 import Navbar from "./components/Navbar";
@@ -10,20 +12,42 @@ import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#757ce8",
+      main: "#3f50b5",
+      dark: "#002884",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000"
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signup} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
