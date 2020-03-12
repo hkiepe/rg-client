26cd3ee0655490be556a27898faf801bfc8974d3
Henrik Kiepe <hkiepe@inkontor.com>
2020-03-12 10:24:46 +0100

    deploy to firebase

--- src/redux/store.js
+++ src/redux/store.js
@@ -15,10 +15,10 @@ const reducers = combineReducers({
   UI: uiReducer
 });
 
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
+const composeEnhancers = compose
+  // typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  //   ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+  //   : compose;
 
 const enhancer = composeEnhancers(applyMiddleware(...middleware));
 const store = createStore(reducers, initialState, enhancer);
